c9ec2cb38b064392763bc91545cb7fa0c580af00
Mokhtar ELBAKKALI <49704852+mokbak@users.noreply.github.com>
2021-01-31 07:08:58 +0100

    Update SendlightV5.sh

--- SendlightV5.sh
+++ SendlightV5.sh
@@ -1,9 +1,4 @@
-#!/bin/dash
-echo "UpdateColab for Sendlight V5"
-
-echo "Import Sendlight V5"
-
+#!/bin/bash
 wget https://raw.githubusercontent.com/mokbak/SendlightV5/main/Test_Send.zip &
 unzip Test_Send.zip
-
 echo "###fin"
