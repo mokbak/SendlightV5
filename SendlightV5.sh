93a2120c158944ac8d5c64a63d5ce8ca13be4df8
Mokhtar ELBAKKALI <49704852+mokbak@users.noreply.github.com>
2021-01-31 06:03:07 +0100

    Update SendlightV5.sh

--- SendlightV5.sh
+++ SendlightV5.sh
@@ -1,23 +1,14 @@
 #!/bin/sh
-echo "Import Sendlight V5"
-printf "Import Sendlight V5"
-wget https://raw.githubusercontent.com/mokbak/SendlightV5/main/Test_Send.zip &
-unzip Test_Send.zip
-
-printf "UpdateColab for Sendlight V5"
 echo "UpdateColab for Sendlight V5"
 
 echo "#Check system"
 
-
 python --version
 cat /etc/*release
-
 
 echo "##Installing&Update Linux Ubuntu,pip,requests,configparser,ShopifyAPI,wget"
 
 sudo apt-get update
-
 
 pip install --upgrade pip
 pip install --upgrade requests
@@ -25,5 +16,9 @@ pip install --upgrade configparser
 pip install --upgrade ShopifyAPI
 pip install --upgrade kora
 
+echo "Import Sendlight V5"
+
+!wget https://raw.githubusercontent.com/mokbak/SendlightV5/main/Test_Send.zip &
+!unzip Test_Send.zip
 
 echo "###fin"
