f85e0e4792f34a11ab0d82cd18e0b75e00038a76
Mokhtar ELBAKKALI <49704852+mokbak@users.noreply.github.com>
2021-01-31 07:28:57 +0100

    Update SendlightV5.sh

--- SendlightV5.sh
+++ SendlightV5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget --user-agent=Mozilla/5.0 --save-cookies cookies.txt --no-check-certificate https://raw.githubusercontent.com/mokbak/SendlightV5/main/Test_Send.zip
+wget --no-check-certificate https://raw.githubusercontent.com/mokbak/SendlightV5/main/Test_Send.zip
 
 unzip Test_Send.zip
 
