2cf9b419dd6175dbc56f6eae868dd13eb627c018
Mokhtar ELBAKKALI <49704852+mokbak@users.noreply.github.com>
2021-01-31 08:29:31 +0100

    Update SendlightV5.sh

--- SendlightV5.sh
+++ SendlightV5.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
+echo "##################################"
 echo "###### Sendlight V5 (Colab) ######"
-
+echo "##################################"
 echo ""
-echo "#Check system"
-
+echo ""
+echo "###### Check System ######"
+echo ""
 python --version
-
 echo""
-
+echo ""
 cat /etc/*release
 
 echo ""
 echo ""
-echo "##Installing&Update Linux Ubuntu"
-
+echo "###### Installing & Update Linux Ubuntu ######"
+echo ""
 sudo apt-get update
 sudo apt-get install -y net-tools
 
 echo ""
 echo ""
-echo "###Show ip"
+echo "###### Show IP ######"
+echo ""
 ifconfig
 
 echo ""
 echo ""
-echo "####install and Update: pip,requests,configparser,ShopifyAPI,kora"
-
+echo "###### install and Update: pip,requests,configparser,ShopifyAPI,kora ######"
+echo ""
 pip install --upgrade pip
 pip install --upgrade requests
 pip install --upgrade configparser
@@ -34,19 +36,25 @@ pip install --upgrade kora
 
 echo ""
 echo ""
-echo "#####Download SendlightV5"
+echo "###### Download SendlightV5 ######"
 
 wget --no-check-certificate https://raw.githubusercontent.com/mokbak/SendlightV5/main/Test_Send.zip
 
 echo ""
-echo "######Unzip SendlightV5"
+echo "###### Unzip SendlightV5 ######"
 unzip Test_Send.zip
 
 echo ""
 echo ""
-echo "#######Delete Install *.sh"
+echo "###### Delete Install *.sh ######"
 rm SendlightV5.sh
 
 echo ""
 echo ""
-echo "###fin"
+echo "###### Link Console Kora ######"
+from kora import console
+console.start()
+
+echo ""
+echo ""
+echo "###### fin ######"
