ad6d57e5a8c556173d6f5333b87cfbbd0921360c
Mokhtar ELBAKKALI <49704852+mokbak@users.noreply.github.com>
2021-02-04 02:22:29 +0100

    Update Unzip_SendlightV5.sh

--- Unzip_SendlightV5.sh
+++ Unzip_SendlightV5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo $(cat Password.ini) is your password.
+echo $(head -1 Password.ini) is your password.
 echo
-unzip -P $(cat Password.ini) Sendlight_V5_LinuxColab.zip
+unzip -P $(head -1 Password.ini) Sendlight_V5_LinuxColab.zip
 
 echo -e j "\n\n"
